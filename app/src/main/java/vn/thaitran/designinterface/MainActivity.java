6d3bb77b996cf328e98dbede30429c6101fd3588
DESKTOP-2PFPM2E\TranThai <thaitran101096@gmail.com>
2019-03-04 22:48:47 +0700

    edit main activity

--- app/src/main/java/vn/thaitran/designinterface/MainActivity.java
+++ app/src/main/java/vn/thaitran/designinterface/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+///NNe,,
     DrawerLayout  drawerLayout;
     private Toolbar toolbar;
     @Override
